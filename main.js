3a3b5f03945dff0e79ac831bdd572780feb6b4e4
Viet <vanvietnc99@gmail.com>
2023-04-26 10:19:01 +0700

    viet

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ const renderdate = () => {
 
   // tính cách ngày trong tháng này 
   for (let i = 1; i <= lastDay; i++){
-    if(i===new Date().getDate() && date.getMonth() === new Date().getMonth()){
+    if(i===new Date().getDate() && date.getMonth() === new Date().getMonth() && date.getFullYear()=== new Date().getFullYear()){
       days += `<div class="today">${i}</div>`
     } else {
       days += `<div class="clickday">${i}</div>`
